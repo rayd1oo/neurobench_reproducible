5ab8eca7ea542f78bf8285ab6c4d6764556f9011
wang-zhuoran <wangzhuoran200105@126.com>
2024-03-17 18:02:29 +0100

    update

---
+++ download.py
@@ -0,0 +1,14 @@
+from tcn_lib import TCN
+import torch
+import neurobench
+from neurobench.datasets import MSWC_dataset
+
+def main():
+    # print("hello world")
+    mswc_dataset_training = MSWC_dataset.MSWC(root = "MSWC/", 
+        subset = "base", procedure = "training", language = "English" ,download = True)
+    mswc_dataset_training.download()
+    
+
+if __name__ == "__main__":
+    main()
